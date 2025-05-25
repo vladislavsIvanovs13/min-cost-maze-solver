194d8756996204b6efde27ef18da0a0b153e1e2f
vladislavsIvanovs13 <vladislavs.ivanovs_3@edu.rtu.lv>
2025-05-25 04:25:57 +0300

    Extend MazeSolver

--- src/main/java/org/maze/Cell.java
+++ src/main/java/org/maze/Cell.java
@@ -1,22 +1,41 @@
 package org.maze;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 // Lombok library reduces boilerplate code, therefore
-// getters, setters and constructors can be generated using annotations
+// getters and setters can be generated using annotations
 
 /** Each cell contains information about its x and y coordinates (indexes)
- * in matrix, value (number, S, G or X), parent cell to keep track of path
- * and visited cell flag to identify if current cell has already been visited. */
+ * in matrix, value (number, S, G or X), cost (only numeric value),
+ * parent cell to keep track of path or visited cell flag
+ * to identify if current cell has already been visited. */
 @Getter
 @Setter
-@AllArgsConstructor
-public class Cell {
+public class Cell implements Comparable<Cell> {
     private int x;
     private int y;
+    private int cost;
     private char value;
     private Cell parent;
     private boolean visited;
+
+    public Cell(int x, int y, char value, Cell parent, boolean visited) {
+        this.x = x;
+        this.y = y;
+        this.value = value;
+        this.parent = parent;
+        this.visited = visited;
+    }
+
+    public Cell(int x, int y, int cost) {
+        this.x = x;
+        this.y = y;
+        this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Cell o) {
+        return this.cost - o.cost;
+    }
 }
--- src/main/java/org/maze/MazeReader.java
+++ src/main/java/org/maze/MazeReader.java
@@ -11,13 +11,12 @@ public class MazeReader {
         this.fileName = "src/main/resources/" + fileName;
     }
 
-    /** Extracts matrix with cells from given txt file. */
-    public Cell[][] getMaze() throws FileNotFoundException {
+    /** Extracts matrix with values from given txt file. */
+    public char[][] getCharMaze() throws FileNotFoundException {
         Scanner scanner = new Scanner(new File(fileName));
         char[] firstLine = scanner.nextLine().toCharArray();
         int mazeSize = firstLine.length;
         char[][] values = new char[mazeSize][mazeSize];
-        Cell[][] maze = new Cell[mazeSize][mazeSize];
 
         // fills char matrix with values from txt file
         int i = 0;
@@ -28,11 +27,20 @@ public class MazeReader {
         }
         scanner.close();
 
+        return values;
+    }
+
+    /** Converts value matrix to Cell object matrix. */
+    public Cell[][] getMaze() throws FileNotFoundException {
+        char[][] values = getCharMaze();
+        int mazeSize = values.length;
+        Cell[][] maze = new Cell[mazeSize][mazeSize];
+
         // populates maze matrix with cell objects that contain
         // values from char matrix, cell coordinates,
         // references to previous cells and 'visited' flag
-        for (var x = 0; x < values.length; x++)
-            for (var y = 0; y < values[x].length; y++)
+        for (var x = 0; x < maze.length; x++)
+            for (var y = 0; y < maze[x].length; y++)
                 maze[x][y] = new Cell(x, y, values[x][y], null, false);
 
         return maze;
